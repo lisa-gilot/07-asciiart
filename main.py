cebb6d7a87dec9ca6af99ae7343564be2759f2ec
lisa-gilot <lisa.gilot@edu.esiee.fr>
2025-10-03 08:32:34 +0000

    code asciiart

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+'''Fonctions itérative et récursive qui permet d'encoder une chaine de
+caractères par une liste de tuples'''
 #### Imports et définition des variables globales
 
 # Mandatory for the recursive solution to work on large inputs
@@ -9,7 +11,8 @@ sys.setrecursionlimit(2000)
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne de caractères 
+    passée en argument selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -17,14 +20,25 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
+    n = len(s)
+    c = [s[0]]
+    o=[1]
+    k=1
+    while k<n:
+        if s[k] == s[k-1]:
+            o[-1] += 1
+        else:
+            o.append(1)
+            c.append(s[k])
+        k += 1
+        l=list(zip(c,o))
+    return l
 
-    return [ ]
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant une chaîne de 
+    caractères passée en argument selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -32,20 +46,27 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
     # cas de base
     # recherche nombre de caractères identiques au premier
     # appel récursif
+    n = len(s)
+    k=1
+    if s == '':
+        return []
+    while k<n and s[k] == s[0]:
+        k += 1
+    sr = s[k:]
+    return [(s[0],k)] + artcode_r(sr)
 
-    return []
-    
+
 
 #### Fonction principale
 
 
 def main():
+    '''
+    tester les fonctions artcode_i et artcode_r
+    '''
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
